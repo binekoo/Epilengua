df80ec9282ce0995cd4c4b0d60eaa7c7710feb3a
Nawel <ghazal.nawel@gmail.com>
2024-02-23 15:50:52 +0100

    Ajout d'une classe GameBuild
    Classe qui va permettre de construire le jeu. Elle a en attributs un set de questions.

---
+++ Epilengua/GameBuild.java
@@ -0,0 +1,9 @@
+package Epilengua;
+
+public class GameBuild {
+    private GameSet gameSet;
+
+    public GameBuild(GameSet gameSet){
+        this.gameSet = gameSet;
+    }
+}
--- Epilengua/GameSet.java
+++ Epilengua/GameSet.java
@@ -6,5 +6,4 @@ public class GameSet {
     public GameSet(Question... question) {
         this.questions = question;
     }
-
 }
