f2fabe36dfc1fc045b61c06e9b67f48d4061c3af
Nawel <ghazal.nawel@gmail.com>
2024-02-23 15:34:34 +0100

    Ajout des classes Question et Reponse

---
+++ Epilengua/Question.java
@@ -0,0 +1,17 @@
+package Epilengua;
+
+public class Question {
+    private String question;
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public Question(String question){
+        this.question = question;
+    }
+}
---
+++ Epilengua/Reponse.java
@@ -0,0 +1,12 @@
+package Epilengua;
+
+public class Reponse {
+    private boolean correcte;
+    private String texte;
+
+    public void setCorrecte(boolean correcte, String texte) {
+        this.correcte = correcte;
+        this.texte = texte;
+    }
+
+}
